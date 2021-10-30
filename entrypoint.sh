0bebdc2c5e8b6887bdab69b566835c2929afe7a6
Shatilov789 <realsteal1223@gmail.com>
2021-10-30 22:33:21 +0300

    Docker_v1.1

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,5 @@ python manage.py makemigrations --no-input
 
 python manage.py migrate --no-input
 
-python manage.py createsuperuser
-
 python manage.py runserver 0.0.0.0:8000
+gunicorn ljingo.wsgi -c ./gunicorn/gunicorn.py -b 0.0.0.0:8000
--- stocks_products/settings.py
+++ stocks_products/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'stocks_products.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'prod_db',
         'USER': 'admin',
         'PASSWORD': 'shatia',
